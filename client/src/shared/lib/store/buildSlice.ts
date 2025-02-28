2f3843e8c0780027874d2baa4c9132d87fc4c078
AlexSereb <aserebrakov343@gmail.com>
2025-02-28 22:58:31 +0300

    gg wp ff

---
+++ client/src/shared/lib/store/buildSlice.ts
@@ -0,0 +1,22 @@
+import {bindActionCreators, CreateSliceOptions, SliceCaseReducers} from "@reduxjs/toolkit/dist";
+import {createSlice} from "@reduxjs/toolkit";
+import {useDispatch} from "react-redux";
+import {useMemo} from "react";
+
+export function buildSlice<
+    State,
+    CaseReducers extends SliceCaseReducers<State>,
+    Name extends string = string
+>(options: CreateSliceOptions<State, CaseReducers, Name>) {
+    const slice = createSlice(options);
+
+    const useActions = () => {
+        const dispatch = useDispatch();
+
+        return useMemo(() => bindActionCreators(slice.actions, dispatch), [dispatch])
+    }
+
+    return {
+        ...slice,
+    };
+}
