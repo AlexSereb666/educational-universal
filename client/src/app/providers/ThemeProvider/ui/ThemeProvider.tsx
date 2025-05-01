2fad12f549f865da4c366ce1d6546df10c0641b5
AlexSereb <aserebrakov343@gmail.com>
2025-05-01 22:42:46 +0300

    add system theme

--- client/src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ client/src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from 'react';
+import { ReactNode, useEffect, useMemo } from 'react';
 import { useJsonSettingByKey } from '@/entities/User';
 import { Theme } from '@/shared/const/themes';
 
@@ -8,7 +8,18 @@ interface ThemeProviderProps {
 
 export const ThemeProvider = (props: ThemeProviderProps) => {
     const { children } = props;
-    const theme = useJsonSettingByKey('theme') || Theme.light;
+
+    const systemTheme: Theme = useMemo(() => {
+        if (typeof window !== 'undefined' && window.matchMedia) {
+            return window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? Theme.dark
+                : Theme.light;
+        }
+
+        return Theme.light;
+    }, []);
+
+    const theme = useJsonSettingByKey('theme') || systemTheme;
 
     useEffect(() => {
         document.documentElement.setAttribute('data-theme', theme);
